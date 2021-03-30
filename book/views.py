0a97854e4682f1b9b3079969104d4b200f11b565
waldemarantypov <78414052+waldemarantypov@users.noreply.github.com>
2021-03-30 18:50:11 +0300

    Update views.py

--- book/views.py
+++ book/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
-import random
+
 
 # Create your views here.
 def show_books(request):
-    allBooks = Book.objects.all()
     randomBooks = Book.objects.all().order_by('?')[:3]
     context = {"allBooks": randomBooks}
     return render(request, 'index.html', context)
